61b3a7fffeeeb2bb09fbe3e8dc92eee2e7ea9c42
evaneill <evaneill@mac.com>
2020-06-20 15:33:50 +0100

    update agent measures.

--- agents/KSampleGoalHeuristicUCTAgent.cpp
+++ agents/KSampleGoalHeuristicUCTAgent.cpp
@@ -65,7 +65,7 @@ Actions::Action* Agents::KSampleGoalHeuristicUCTAgent::generate_action(bool verb
 
     tree_depths.push_back(search_depth); // Maximum search depth at this step
     chosen_rewards.push_back((double) chosen_child -> TotalReward / (double) chosen_child -> N_visits); // "Expected Reward"
-    chosen_confidences.push_back(chosen_child ->score - ((double) chosen_child -> TotalReward / (double) chosen_child -> N_visits)); // "Exploration term" = upper confidence interval size
+    chosen_confidences.push_back(Search::UCB1Score(chosen_child)-((double) chosen_child -> TotalReward / (double) chosen_child -> N_visits)); // "Exploration term" = upper confidence interval size; avoiding calculating it for ourselves.
     chosen_visits_minus_avg.push_back((
         (
             (double) chosen_child -> N_visits) - ((double) n_simulations/(double)chosen_child -> parent -> n_children()) 
