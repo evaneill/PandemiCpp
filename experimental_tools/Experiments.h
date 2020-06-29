fc4bb7cfb21310498877cbfaf2aa46a4d716b6ec
evaneill <evaneill@mac.com>
2020-06-29 17:42:22 +0100

    try new experiment - try hella simulations/step and maybe it wins?

--- experimental_tools/Experiments.h
+++ experimental_tools/Experiments.h
@@ -690,4 +690,25 @@ namespace Experiments
         std::vector<Measurements::GameMeasurement*> get_game_measures(Board::Board* board);
         void reset_board(Board::Board* game_board);
     };
+
+    class K3_250k_AStar_CurePrecondition_UCTMaxChildExperiment: public Experiment {
+    public:
+        K3_250k_AStar_CurePrecondition_UCTMaxChildExperiment();
+        ~K3_250k_AStar_CurePrecondition_UCTMaxChildExperiment(){
+            delete scenario;
+            for(Measurements::MeasurementConstructor* cons: measureCons){
+                delete cons;
+            }
+            measureCons.clear();
+        };
+
+        void write_header();
+        void append_header(std::string extras);
+        void write_experiment(std::string data);
+
+        Board::Board* get_board();
+        Agents::BaseAgent* get_agent(GameLogic::Game* game);
+        std::vector<Measurements::GameMeasurement*> get_game_measures(Board::Board* board);
+        void reset_board(Board::Board* game_board);
+    };
 }
