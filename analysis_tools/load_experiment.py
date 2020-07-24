a45e3ed42a016bc75a4070010c241f904ba1a911
evaneill <evaneill@mac.com>
2020-07-24 08:20:57 +0100

    update analysis tools for loading multiple experiments in a command

--- analysis_tools/load_experiment.py
+++ analysis_tools/load_experiment.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 
+import os
+
 def featurizeEventUsage(df):
     # Instantiate new column for how long airlift was present before use, if used (-1 if either never present or present & not used)
     df['AirliftUseTime']=df.AirliftUse.copy()
@@ -16,6 +18,15 @@ def featurizeEventUsage(df):
     df['QuietNightUseTime']=df.QuietNightUse.copy()
     df.QuietNightUseTime[df.QuietNightUseTime>=0] = df.QuietNightUseTime[df.QuietNightUseTime>=0] - df.firstQuietNightPresence[df.QuietNightUseTime>=0]
     df.QuietNightUseTime[df.QuietNightUseTime<0] = np.nan
+
+    # reset to np.nan any values that were -1 (indicating never seen and/or never used)
+    df.loc[df.firstAirliftPresence==-1,'firstAirliftPresence']=np.nan
+    df.loc[df.firstGovernmentGrantPresence==-1,'firstGovernmentGrantPresence']=np.nan
+    df.loc[df.firstQuietNightPresence==-1,'firstQuietNightPresence']=np.nan
+
+    df.loc[df.AirliftUse==-1,'AirliftUse']=np.nan
+    df.loc[df.GovernmentGrantUse==-1,'GovernmentGrantUse']=np.nan
+    df.loc[df.QuietNightUse==-1,'QuietNightUse']=np.nan
     
 def featurizeCure(df):
     df['n_Cured']=1*(df.BlueCured>0) + 1*(df.YellowCured>0) + 1*(df.BlackCured>0) + 1*(df.RedCured>0)
@@ -52,10 +63,11 @@ def series_compare(df1,df2):
 
     return comparison_series
 
-def load_df(fpath,K,sims,heuristic):
+def load_df(fpath,K,sims,heuristic,selection="Greedy Expectimax"):
     # fpath (str)   = filepath to experiment (e.g. "results/<>.csv")
     # K (int)       = number of determinizations (e.g. 3)
     # sims (str)    = simulations per step (e.g. "10k")
+    # selection (str) (optional) = selection policy used by agent (default "Greedy Expectimax") 
 
     df = pd.read_csv(fpath)
     featurize(df)
@@ -63,6 +75,36 @@ def load_df(fpath,K,sims,heuristic):
     df['Heuristic']=heuristic
     df['Determinizations']=K
     df['nSims']=sims
+    df['Selection Policy']=selection
 
     return df
 
+def load_dfs(suffix,K,N,heuristic,fpath="./",selection_policy="Greedy Expectimax"):
+    # A function to collect and return a concatenated dataframe of a collection of experiments that all share a common suffix
+    # Will try collecting ALL of the possible pairs of K and N
+    #
+    # fpath = relative or absolute filepath to collection of experiment folders (e.g. <fpath>/K1_10k_..../*.csv)
+    # suffix = end of experiment-holding folder filename (e.g. <fpath>/K1_10k_<suffix>/*.csv)
+    # K = list of ints or strs indicating which values of K to collect (e.g. K=[1,3,5,10,20])
+    # N = list of strs representing the number of simulations in the experiment (e.g. N = ["500","10k","50k"])
+
+    dfs = []
+    for k in K:
+        for n in N:
+            folder_name = f"K{k}_{n}_"+suffix+"/"
+            full_fpath = os.path.join(fpath,folder_name)
+            
+            experiment_files = os.listdir(full_fpath)
+            if len(experiment_files)==2:
+
+                for exp_file in experiment_files:
+                    if exp_file.startswith(f"K{k}_{n}_"):
+                        if exp_file.endswith(".csv"):
+                            temp_df = load_df(os.path.join(full_fpath,exp_file),k,n,heuristic,selection_policy)
+                            dfs.append(temp_df)
+                    else:
+                        raise Exception
+            else:
+                raise Exception
+
+    return pd.concat(dfs,axis=0)
